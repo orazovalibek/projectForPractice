2cfec46172f423f3745bd73ece4113d662f78927
orazovalibek <orazalike@gmail.com>
2020-11-03 14:09:00 -0500

    third commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -1,2 +1,5 @@
 public class Git {
+    public static void main(String[] args) {
+        System.out.println("We learning GitHub");
+    }
 }
--- src/test/java/GitHub.java
+++ src/test/java/GitHub.java
@@ -1,5 +1,5 @@
 public class GitHub {
     public static void main(String[] args) {
-
+        System.out.println("We are learning Git");
     }
 }
